12b98e302a019d49ac99bebeaf6e50f30b4bdfcf
bhavvzs <bhavanareddy240205@gmail.com>
2024-12-08 22:46:57 +0530

    first

---
+++ One_to_Hundered.java
@@ -0,0 +1,18 @@
+public class One_to_Hundered {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100 are");
+        for(int n=2;n<=100;n++){
+            boolean isPrime=true;
+            for(int i=2;i<=n/2;i++){
+                if(n/i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(n);
+            }
+        }
+    }
+}
+
